337562fe4b740396bc36ff4342bfb12321746e32
rimtimti <rimtimti@mail.ru>
2022-09-24 19:12:58 +0300

    дз к семинау 6+

--- Lesson06/Task0.py
+++ Lesson06/Task0.py
@@ -1,92 +1,90 @@
-# Напишите программу вычисления арифметического выражения заданного строкой.
-# Используйте операции +,-,/,*. приоритет операций стандартный.
-# Дополнительное задание: Добавьте возможность использования скобок, меняющих приоритет операций
-# *Пример:*
-# 2+2 => 4;
-# 1+2*3 => 7;
-# 10/2*5 => 25;
-# 10 * 5 * => недостаточно числовых данных
-# -5 + 5 => 0
-# два + три => неправильный ввод: нужны числа
-# (2+((5-3)*(16-14)))/3 => 2
-# (256 - 194 => некорректная запись скобок
+import os
+os.system('cls')
+print('Программа вычисляет арифметическое выражение, заданное строкой.')
 
-def check(s: str) -> str:
-    count_1 = 0
-    count_2 = 0
-    for symbol in s:
+def check_correct_input(text: str) -> str:
+    '''
+    Проверяет правильность ввода арифметического выражения
+    '''
+    for symbol in text:
         if symbol.isalpha():
             print('Неправильный ввод: нужны числа')
             exit()
-    for i in range(len(s)):
-        if s[i] == '(':
-            count_1 += 1
-        if s[i] == ")":
-            count_2 += 1
-    if count_1 != count_2:
+    for symbol in range(len(text)):
+        count_left = 0
+        count_right = 0
+        if symbol == '(':
+            count_left += 1
+        if symbol == ')':
+            count_right += 1
+    if count_left != count_right:
         print("Некорректная запись скобок")    
         exit()
-    if s[-1] == '+' or s[-1] == '-' or s[-1] == '*' or s[-1] == '/':
+    if text[-1] == '+' or text[-1] == '-' or text[-1] == '*' or text[-1] == '/' or text[-1] == '(':
         print("Недостаточно числовых данных")
         exit()
-    return s
+    return text
 
-def parse(s: str) -> list:
+def parse_into_numbers_and_signs(text: str) -> list:
+    '''
+    Разбирает арифметическое выражение в виде чисел и знаков
+    '''
     result = []
     digit = ""
-    for symbol in s:
-        if symbol.isdigit():
+    for symbol in text:
+        if symbol.isdigit() or symbol == '.':
             digit += symbol
-        elif symbol in ['(', ')']:
+        else:
             if digit:
                 result.append(float(digit))
                 digit = ""
             result.append(symbol)
-        else:
-            if digit:
-                result.append(float(digit))
-            digit = ""
-            result.append(symbol)
-    if digit:
-        result.append(float(digit))
+        # else:
+        #     if digit:
+        #         result.append(float(digit))
+        #     digit = ""
+        #     result.append(symbol)
+    # if digit:
+    #     result.append(float(digit))
     if result[0] == '-':
         result.remove(result[0])
-        result[0] = -result[0]
+        result[0] = -1*result[0]
     return result
 
-def calculator(lst: list) -> float:
-    result = 0.0
-    while '/' in lst:
-        index = lst.index('/')
-        result = lst[index - 1] / lst[index + 1]
-        lst = lst[:index - 1] + [result] + lst[index + 2:]
-    while '*' in lst:
-        index = lst.index('*')
-        result = lst[index - 1] * lst[index + 1]
-        lst = lst[:index - 1] + [result] + lst[index + 2:]
-    while '/' in lst:
-        index = lst.index('/')
-        result = lst[index - 1] / lst[index + 1]
-        lst = lst[:index - 1] + [result] + lst[index + 2:]
-    while '+' in lst:
-        index = lst.index('+')
-        result = lst[index - 1] + lst[index + 1]
-        lst = lst[:index - 1] + [result] + lst[index + 2:]
-    while '-' in lst:
-        index = lst.index('-')
-        result = lst[index - 1] - lst[index + 1]
-        lst = lst[:index - 1] + [result] + lst[index + 2:]
+def calculator(array_numbers_and_sings: list) -> float:
+    '''
+    Вычисление выражений
+    '''
+    result = 0
+    while '/' in array_numbers_and_sings:
+        index = array_numbers_and_sings.index('/')
+        result = array_numbers_and_sings[index - 1] / array_numbers_and_sings[index + 1]
+        array_numbers_and_sings = array_numbers_and_sings[:index - 1] + [result] + array_numbers_and_sings[index + 2:]
+    while '*' in array_numbers_and_sings:
+        index = array_numbers_and_sings.index('*')
+        result = array_numbers_and_sings[index - 1] * array_numbers_and_sings[index + 1]
+        array_numbers_and_sings = array_numbers_and_sings[:index - 1] + [result] + array_numbers_and_sings[index + 2:]
+    while '+' in array_numbers_and_sings:
+        index = array_numbers_and_sings.index('+')
+        result = array_numbers_and_sings[index - 1] + array_numbers_and_sings[index + 1]
+        array_numbers_and_sings = array_numbers_and_sings[:index - 1] + [result] + array_numbers_and_sings[index + 2:]
+    while '-' in array_numbers_and_sings:
+        index = array_numbers_and_sings.index('-')
+        result = array_numbers_and_sings[index - 1] - array_numbers_and_sings[index + 1]
+        array_numbers_and_sings = array_numbers_and_sings[:index - 1] + [result] + array_numbers_and_sings[index + 2:]
     return result
 
-def brackets(lst: list) -> list:
-    while '(' in lst:
-        close = lst.index(')')
-        temp_lst = [y for x, y in zip(lst[:close], list(range(close-1))) if x == '(']
-        open = temp_lst[-1]        
-        lst = lst[:open] + [calculator(lst[open + 1: close])] + lst[close + 1:]
-    return lst
+def priority_brackets(array_numbers_and_sings: list) -> list:
+    '''
+    Вычисление выражений с учетом приоритета скобок
+    '''
+    while '(' in array_numbers_and_sings:
+        close = array_numbers_and_sings.index(')')
+        # temp = [y for x, y in zip(array_numbers_and_sings[:close], list(range(close-1))) if x == '(']
+        open = [y for x, y in zip(array_numbers_and_sings[:close], list(range(close-1))) if x == '('][-1]        
+        array_numbers_and_sings = array_numbers_and_sings[:open] + [calculator(array_numbers_and_sings[open + 1: close])] + array_numbers_and_sings[close + 1:]
+    return array_numbers_and_sings
 
-expression = '(2+((5-3)*(16-14)))/3'
-x = ''.join(expression.split())
-result = brackets(parse(check(x)))
+temp = ''.join(input('Введите математическое выражение: ').split())
+result = priority_brackets(parse_into_numbers_and_signs(check_correct_input(temp)))
 print(calculator(result))
